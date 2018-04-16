bbd49334d7a355164f6070ee62cb644a9bdc067e
LenLenLenLenLen <lenabjekovic@hotmaill.com>
2018-04-16 22:56:55 +0200

    Participants html

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -24,4 +24,24 @@ console.log('Sanity Check: 200 + 223 = ' + calculation);
 console.log('bye.');
 "use strict";
 
-var navigation = document.getElementById("navigation");
+var nav = document.getElementById("navigation");
+var weather = document.getElementById("weather");
+var balloon = document.getElementById("balloon");
+window.addEventListener("scroll", function () {
+
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 45) {
+        nav.style.padding = "0px";
+        nav.style.transform = "translate(0px,-20px)";
+        balloon.style.height = "80px";
+        weather.style.padding = "0px";
+        nav.style.borderBottom = "1px solid gray";
+    } else {
+        nav.style.padding = '3px';
+        balloon.style.height = "100px";
+        weather.style.padding = "10px";
+        nav.style.transform = "translate(0px,0px)";
+        nav.style.borderBottom = "none";
+    }
+});
--- scripts/domain/main.js
+++ scripts/domain/main.js
@@ -1,2 +1,24 @@
-var navigation = document.getElementById("navigation");
+var nav = document.getElementById("navigation");
+var weather = document.getElementById("weather");
+var balloon = document.getElementById("balloon");
+window.addEventListener("scroll",function(){
+
+    var scrollPosition = window.scrollY;
+
+    if(scrollPosition > 45){
+      nav.style.padding="0px";
+      nav.style.transform="translate(0px,-20px)";
+      balloon.style.height="80px";
+      weather.style.padding="0px";
+      nav.style.borderBottom="1px solid gray";
+
+    } else{
+        nav.style.padding='3px';
+        balloon.style.height="100px";
+        weather.style.padding="10px";
+        nav.style.transform="translate(0px,0px)";
+        nav.style.borderBottom="none";
+    }
+    
+})
 
