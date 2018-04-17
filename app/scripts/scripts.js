aadc7c647bef120a5db2c9553925450327ab3d4d
strba89 <strbo89@yahoo.com>
2018-04-17 16:08:37 +0200

    contact us

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -13,15 +13,6 @@ function markoFunkcija(args) {
 
     return true;
 }
-'use strict';
-
-console.log('I have entered this file.');
-console.log('This is crystal clear evidence that this works.');
-
-var calculation = 200 + 223;
-
-console.log('Sanity Check: 200 + 223 = ' + calculation);
-console.log('bye.');
 // var nav = document.getElementById("navigation");
 // var weather = document.getElementById("weather");
 // var balloon = document.getElementById("balloon");
