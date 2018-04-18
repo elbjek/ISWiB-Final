7780f450856da22ca9660c0417ca07bcb9553626
elbjek <miovcic3@gmail.com>
2018-04-18 10:38:58 +0200

    lol

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -13,27 +13,11 @@ function markoFunkcija(args) {
 
     return true;
 }
-// var nav = document.getElementById("navigation");
-// var weather = document.getElementById("weather");
-// var balloon = document.getElementById("balloon");
-// window.addEventListener("scroll",function(){
+'use strict';
 
-//     var scrollPosition = window.scrollY;
+var nav = document.getElementById('navigation');
 
-//     if(scrollPosition > 45){
-//       nav.style.padding="0px";
-//       nav.style.transform="translate(0px,-20px)";
-//       balloon.style.height="80px";
-//       weather.style.padding="0px";
-//       nav.style.borderBottom="1px solid gray";
+window.addEventListener('scroll', function () {
 
-//     } else{
-//         nav.style.padding='3px';
-//         balloon.style.height="100px";
-//         weather.style.padding="10px";
-//         nav.style.transform="translate(0px,0px)";
-//         nav.style.borderBottom="none";
-//     }
-
-// })
-"use strict";
+  console.log(nav.offsetTop);
+});
--- scripts/domain/main.js
+++ scripts/domain/main.js
@@ -1,25 +1,7 @@
-// var nav = document.getElementById("navigation");
-// var weather = document.getElementById("weather");
-// var balloon = document.getElementById("balloon");
-// window.addEventListener("scroll",function(){
+var nav = document.getElementById('navigation');
 
-//     var scrollPosition = window.scrollY;
+window.addEventListener('scroll',function(){
 
-//     if(scrollPosition > 45){
-//       nav.style.padding="0px";
-//       nav.style.transform="translate(0px,-20px)";
-//       balloon.style.height="80px";
-//       weather.style.padding="0px";
-//       nav.style.borderBottom="1px solid gray";
+console.log(nav.offsetTop);
 
-//     } else{
-//         nav.style.padding='3px';
-//         balloon.style.height="100px";
-//         weather.style.padding="10px";
-//         nav.style.transform="translate(0px,0px)";
-//         nav.style.borderBottom="none";
-//     }
-    
-// })
-
-
+})
